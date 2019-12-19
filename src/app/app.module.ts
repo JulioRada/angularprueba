3d7e125a06cd58ebbb995abfb27f7c0c5efa0e90
Julio Cesar Rada <jcrada@bg.com.bo>
2019-12-19 19:32:56 -0400

    Captcha, cosas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgxCaptchaModule } from 'ngx-captcha';
 
 import { AppComponent } from './app.component';
-
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { NgxTrimModule } from 'ngx-trim';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    FormsModule,
+    NgxCaptchaModule,
+    NgxTrimModule
   ],
   providers: [],
   bootstrap: [AppComponent]
